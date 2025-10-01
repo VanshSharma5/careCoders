00e8527f1ecef2831da4b6e5029d3258cc44168b
VanshSharma5 <vanshsharmamzn34@gmail.com>
2025-10-01 21:29:47 +0530

    rename application

--- src/main/java/com/_1shhub/carecoders/CareCoders.java
+++ src/main/java/com/_1shhub/carecoders/CareCoders.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedilockerApplication {
+public class CareCoders {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedilockerApplication.class, args);
+		SpringApplication.run(CareCoders.class, args);
 	}
 
 }
