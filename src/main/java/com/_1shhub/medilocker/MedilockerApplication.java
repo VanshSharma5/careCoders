ef5e822fa4473872b79fcdc41bf1551d323b298a
VanshSharma5 <vanshsharmamzn34@gmail.com>
2025-10-01 18:33:21 +0530

    Create Project

---
+++ src/main/java/com/_1shhub/medilocker/MedilockerApplication.java
@@ -0,0 +1,13 @@
+package com._1shhub.medilocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_1shhub/medilocker/MedilockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._1shhub.medilocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
